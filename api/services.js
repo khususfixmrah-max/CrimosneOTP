7fce232ec4cf1e8019887dea4d617704c985cb34
khususfixmrah-max <khususfixmrah@gmail.com>
2026-09-03 01:21:57 +0900

    Update services.js

--- api/services.js
+++ api/services.js
@@ -1,1 +1,5 @@
-
+const {json,rumah}=require("./_lib");
+module.exports=async(req,res)=>{
+  try{return json(res,200,await rumah("v2/services"))}
+  catch(e){return json(res,502,{success:false,error:{message:e.message}})}
+};
